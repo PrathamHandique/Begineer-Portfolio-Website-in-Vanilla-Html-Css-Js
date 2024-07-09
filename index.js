55b57e6f936472b7409fd414b690d9e87c082000
Pratham Handique <104254619+PrathamHandique@users.noreply.github.com>
2024-07-09 23:25:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.getElementById('projects-link').addEventListener('click', function(event) {
+event.preventDefault();
+document.getElementById('projects-heading').scrollIntoView({ behavior: 'smooth' });
+});
